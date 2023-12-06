3af40a99a575234733d5cf2572692cd71d17a8c9
Irfan2309 <90820501+Irfan2309@users.noreply.github.com>
2023-12-06 12:39:40 +0400

    also_likes.py

---
+++ SourceCode/PythonFiles/Functions/also_likes.py
@@ -0,0 +1,68 @@
+from graphviz import Digraph
+
+# Get the list of all the readers who visited the document
+def get_readers_by_document(data, doc_uuid):
+
+    # Initialize the list to store the readers
+    all_readers = []
+
+    for record in data:
+        # Check if the subject_doc_id is present in the record 
+        # if the subject_doc_id is equal to the doc_uuid
+        # and if the visitor_uuid is present in the record
+        if 'subject_doc_id' in record and  record['subject_doc_id'] == doc_uuid and 'visitor_uuid' in record:
+            all_readers.append(record['visitor_uuid'])
+    return all_readers
+
+# Get the list of all the documents read by the visitor
+def get_document_by_readers(data, visitor_uuid):
+
+    # Initialize the list to store the documents
+    all_docs = []
+
+    for record in data:
+        # Check if the visitor_uuid is present in the record
+        # if the subject_doc_id is present in the record
+        # and if the visitor_uuid is equal to the visitor_uuid
+        if 'visitor_uuid' in record and record['visitor_uuid'] == visitor_uuid and 'subject_doc_id' in record:
+            all_docs.append(record['subject_doc_id'])
+    return all_docs
+
+# Get the top 10 documents that are read by the readers who also read the document
+def also_likes(data, doc_uuid, sorting_function=None):
+
+    # If the sorting function is not provided, then use the default sorting function
+    if sorting_function is None:
+        sorting_function = lambda x: x[1]['count']
+
+    # Get the list of all unique readers who visited the document
+    all_readers = set(get_readers_by_document(data, doc_uuid))
+
+    # Initialize the dictionary to store the documents
+    # The dictionary will have the document id as the key and the value will be a dictionary
+    # The value dictionary will have two keys: count and readers 
+    # count will store the number of readers who read the document 
+    # readers will store the list of all the readers who read the document
+    liked_documents = {}
+    
+    for reader in all_readers:
+            
+            # Get the list of all the documents read by the reader
+            documents = set(get_document_by_readers(data, reader))
+            for doc in documents:
+                # Check if the document is already present in the dictionary
+                if doc not in liked_documents:
+                    # If the document is not present in the dictionary, then add the document to the dictionary
+                    liked_documents[doc] = {'count': 0, 'readers': set()}
+                # Increment the count of the document by 1
+                liked_documents[doc]['count'] += 1
+                # Add the reader to the list of readers who read the document
+                liked_documents[doc]['readers'].add(reader)
+
+    # Sort the documents based on the sorting function
+    sorted_docs = sorted(liked_documents.items(), key=sorting_function, reverse=True)
+
+    # Return the top 10 documents
+    return sorted_docs[:10]
+
+
